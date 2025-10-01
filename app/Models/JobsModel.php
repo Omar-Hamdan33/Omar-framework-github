4aac666a2971856912c99ed7632405d33c3a29b2
Omar-Hamdan33 <oh073827@gmail.com>
2025-10-01 09:24:52 +0200

    mysql gebruiken inplaats van sqlite

--- app/Models/JobsModel.php
+++ app/Models/JobsModel.php
@@ -1,37 +1,23 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
 
-class Jobsmodel {
-    public static function all():  array 
+class JobsModel
+{
+    public static function all(): array
     {
-       $data =[
-            [
-                'id' => 1,
-                'title' => 'Laravel',
-                'logo' => 'https://laravel.com/img/logomark.min.svg',
-                'url' => 'https://laravel.com'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Vue',
-                'logo' => 'https://vuejs.org/images/logo.png',
-                'url' => 'https://vuejs.org'
-            ],
-            [
-                'id' => 3,
-                'title' => 'React',
-                'logo' => 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
-                'url' => 'https://reactjs.org'
-            ]
-        ];
-
-        return $data;
+        return DB::table('job')->get()->toArray();
     }
 
-    public static function find($id): array 
-    {
-    return  Arr::first(static::all(), fn ($job) => $job['id'] == $id) ?? ['id' => null, 'title' => 'Job not found', 'logo' => '', 'url' => ''];
+    public static function find($id)
+{
+    return DB::table('job')->where('id', $id)->first() ?? (object)[
+        'id' => null,
+        'title' => 'Job not found',
+        'logo' => '',
+        'url' => ''
+    ];
 }
+
 }
--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,12 +1,12 @@
 
 <x-layout>
-  @if (!empty($job) && $job['id'] !== null)
+  @if (!empty($job) && $job->id !== null)
     
         <div style="border: 1px solid black; padding: 10px; margin: 10px; width: 20rem; align-items: center; text-align: center;">
             <hr>
-            <img src="{{ $job['logo'] }}" alt="logo" style="width: 50px; height: 50px;" > <hr> </img>
-            <h2><a href="/jobs/{{ $job['id'] }}">{{ $job['title'] }}</a></h2 >
-            <a href="{{ $job['url'] }}">Apply Now</a>
+            <img src="{{ $job->logo }}" alt="logo" style="width: 50px; height: 50px;" > <hr> </img>
+            <h2><a href="/jobs/{{ $job->id }}">{{ $job->title }}</a></h2 >
+            <a href="{{ $job->url }}">Apply Now</a>
             <hr>
         </div>
        
